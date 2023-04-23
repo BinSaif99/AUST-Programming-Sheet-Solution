439bc70a4d6594a1b8f75b016d5626e900f96ea4
Alif Bin Saif <alifbinsaif@gmail.com>
2023-04-24 00:04:20 +0600

    solution 3

---
+++ Problem_Set & Solutions/Solutions/Section A Solutions/Question A3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   int num, square;
+
+   printf("Enter a number: ");
+   scanf("%d", &num);
+
+   square = num * num;
+
+   printf("The square of %d is %d", num, square);
+
+   return 0;
+}
+
